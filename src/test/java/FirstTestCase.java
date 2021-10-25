311c125ac97bfc7b0c8f98708e685344a2e81a69
MoritzSalein <83296020+MoritzSalein@users.noreply.github.com>
2021-10-25 13:31:45 +0200

    Update FirstTestCase.java

--- src/test/java/FirstTestCase.java
+++ src/test/java/FirstTestCase.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 
 public class FirstTestCase extends GeneralHelper {
 
-
     @Test
     @Tag("MyFirstTest")
     public void firstTestCase(){
@@ -12,17 +11,14 @@ public class FirstTestCase extends GeneralHelper {
         generalPage.doOpenBrowserWithLink("http://selenium.webtesting.eu/");
         startPage.checkHeaderContributionsIsShown();
         generalPage.clickOnElementWithId("menu-item-134");
-        Assertions.assertEquals( "TESTSEITE-KONTAKTFORMULAR", driver.findElement(By.xpath("//h1[contains(text(),'Testseite-Kontaktformular')]")).getText());
+        Assertions.assertEquals("TESTSEITE-KONTAKTFORMULAR", driver.findElement(By.xpath("//h1[contains(text(),'Testseite-Kontaktformular')]")).getText());
         contactPage.checkBusinessIsChecked();
         contactPage.checkPrivateIsNotChecked();
     }
-
 
     @Test
     @Tag("MySecondTestcase")
     public void secondTestCase(){
         System.out.println("SecondTest");
     }
-
-
 }
